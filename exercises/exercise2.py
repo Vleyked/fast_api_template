b57d6f369ae88abde2109321defcc5beb99def77
victorcid <vcfiguera@gmail.com>
2023-09-24 13:16:59 +0300

    Exercise 2 solution

---
+++ exercises/exercise2.py
@@ -0,0 +1,15 @@
+"""
+Normal Version:
+Exercise 1: Add a New Entity - Courses
+Objective: Integrate a new entity called "courses" into the application.
+Tasks:
+* Design the SQLite3 table for courses. Each course should have an id, name, description, and instructor.
+* Implement CRUD operations for courses.
+* Add the necessary API endpoints to interact with courses.
+
+Exercise 2: List Students in a Course
+Objective: Show which students are enrolled in a specific course.
+Tasks:
+Design a new SQLite3 table to manage student-course relationships.
+Implement an endpoint to list all students enrolled in a specific course.
+"""
